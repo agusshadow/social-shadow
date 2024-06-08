271730e4ce8f2d0d6a2bde975f3c732de97d56ca
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-06-07 21:40:24 -0300

    New input component

---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: 'Input',
+    emits: ['update:modelValue'],
+    props: { modelValue: String, label: String, type: String }
+}
+
+</script>
+
+<template>
+
+    <label :for="label" class="block sr-only">{{ label }}</label>
+    <input 
+         @input="$emit('update:modelValue', $event.target.value)"
+        :id="label" 
+        :type="type" 
+        :placeholder="label" 
+        class="w-full p-3 mt-1 mb-2 border-2 border-gray-300 rounded-md focus:outline-none focus:border-purple-400"
+    >
+    
+</template>
