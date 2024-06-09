38e7a7565a74137af89431ef9a4b97dd551d152a
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-06-09 11:43:11 -0300

    Move navbar to a new component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,25 @@
 <script>
 
-import { subscribeToAuth } from './services/auth.js'
-import Avatar from './components/Avatar.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
-  components: { Avatar },
-  data() {
-        return {
-            authUser: {
-                id: null,
-                email: null,
-                username: null
-            },
-            loading: true
-        };
-    },
+  components: { Navbar },
     methods: {
-      goToHome() {
-        this.$router.push({
-              path: '/publicaciones'
-          });
+      showNabvar() {
+        return this.$route.path == '/iniciar-sesion' || this.$route.path == '/registro' ? false : true;
       },
-      goToProfile() {
-        this.$router.push({
-              path: `/perfil/${this.authUser.id}`
-          });
-      }
     },
-    mounted() {
-        subscribeToAuth(newUserData => {
-          this.authUser = newUserData
-        });
-    }
 }
 
 </script>
 
 <template>
 
-  <div class="bg-slate-50 fixed top-0 w-full py-5 px-3 z-10">
-    <div class="flex justify-between items-center">
-      <img @click="goToHome()" src="../public/logo-nuevo.svg" alt="" class="w-8 cursor-pointer">
-      <div v-if="authUser.id" @click="goToProfile()" class="cursor-pointer">
-        <Avatar :src="''" :alt="authUser.username" :width="6" :height="6"/>
-      </div>
-    </div>
-  </div>
+  <Navbar v-if="showNabvar()" />
 
   <div class="md:mx-36 pt-20">
     <router-view></router-view>
   </div>
-
-  <Loader v-if="loading" :message="'Cerrando sesion'"/>
 
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<script>
+
+import Avatar from './Avatar.vue';
+import { subscribeToAuth } from '../services/auth';
+
+export default {
+    name: 'Navbar',
+    components: { Avatar },
+    data() {
+        return {
+            authUser: {
+                id: null,
+                email: null,
+                username: null
+            },
+            unsubscribeFromAuth: () => {},
+        };
+    },
+    methods: {
+        goToHome() {
+            this.$router.push({
+                path: '/publicaciones'
+            });
+        },
+        goToProfile() {
+            this.$router.push({
+                path: `/perfil/${this.authUser.id}`
+          });
+        }
+    },
+    mounted() {
+        this.unsubscribeFromAuth = subscribeToAuth(newUserData => this.authUser = newUserData);
+    },
+    unmounted() {
+        this.unsubscribeFromAuth();
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="bg-slate-50 fixed top-0 w-full py-5 px-3 z-10">
+        <div class="flex justify-between items-center">
+          <img @click="goToHome()" src="../../public/logo-nuevo.svg" alt="" class="w-8 cursor-pointer">
+          <div v-if="authUser.id" @click="goToProfile()" class="cursor-pointer">
+            <Avatar :src="''" :alt="authUser.username" :width="6" :height="6"/>
+          </div>
+        </div>
+    </div>
+    
+</template>
