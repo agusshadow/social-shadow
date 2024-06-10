568ffb98dea601fa29d25b1140baa716d30fc8c9
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-06-10 16:44:55 -0300

    Fix primary button border

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -13,13 +13,13 @@ export default {
         <button 
             v-if="buttonType === 'primary'"
             :type="type" 
-            class="w-full p-3 border font-bold rounded-md text-white  bg-purple-700  hover:bg-purple-800 focus:outline-none focus:bg-purple-900">
+            class="w-full p-3 border font-bold rounded-md text-white border-purple-700  bg-purple-700  hover:bg-purple-800 focus:outline-none focus:bg-purple-900">
             <slot></slot>
         </button>
         <button 
             v-if="buttonType === 'secondary'"
             :type="type" 
-            class="w-full p-3 border font-bold rounded-md border-purple-700 text-purple-700 hover:bg-purple-100">
+            class="w-full p-3 border font-bold rounded-md text-purple-700 border-purple-700 hover:bg-purple-100">
             <slot></slot>
         </button>
     </div>
